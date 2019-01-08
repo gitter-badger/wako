3b1dbb2274846d4e6ac2b64e3eb34b72b431bfd4
Jean-Baptiste Malatrasi <jb@wizishop.com>
2019-01-08 18:38:21 +0100

    Remove unused specs

--- src/app/shared/directives/scroll-watcher/scroll-watcher.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ScrollWatcherDirective } from './scroll-watcher.directive';
-
-describe('ScrollWatcherDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ScrollWatcherDirective();
-    expect(directive).toBeTruthy();
-  });
-});
